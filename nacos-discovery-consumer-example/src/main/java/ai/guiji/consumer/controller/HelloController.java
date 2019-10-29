c5e402827b4a5124e62467e47be4050dad9889a4
michaelzhou2016 <strength_zhou@qq.com>
2019-10-29 09:42:54 +0800

    使用Nacos作为配置中心

--- nacos-discovery-consumer-example/src/main/java/ai/guiji/consumer/controller/HelloController.java
+++ nacos-discovery-consumer-example/src/main/java/ai/guiji/consumer/controller/HelloController.java
@@ -2,17 +2,28 @@ package ai.guiji.consumer.controller;
 
 import ai.guiji.consumer.remote.HelloRemote;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
+    @Value("${my.name}")
+    private String myname;
+
     @Autowired
     private HelloRemote helloRemote;
 
     @RequestMapping("/hello/{name}")
     public String index(@PathVariable("name") String name) {
         return helloRemote.hello(name);
+    }
+
+    @RequestMapping("/my/name")
+    public String getMyname() {
+        return myname;
     }
 }
